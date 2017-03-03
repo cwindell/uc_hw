50770a8ea8a30276ae7d54ca9713705f76694179
Caroline Windell <cwindell@gmail.com>
2017-03-03 11:41:24 -0800

    Update final.js

--- final/final.js
+++ final/final.js
@@ -5,7 +5,7 @@ document.addEventListener ('DOMContentLoaded', function (){
 	var divReg = document.getElementById('divRegistered');
 	var divMain = document.getElementById('divMain');
 	
-	divMain.addEventListener('mouseover', function (event) {fHandleEnter(event, this);}, true);
+	divMain.addEventListener('click', function (event) {fHandleEnter(event, this);}, true);
 	divMain.addEventListener('mouseout', function (event) {fHandleExit(event, this);}, true);
 
 });

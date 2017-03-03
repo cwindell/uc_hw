15847b07375178f7314cd2f846dfccd3491b0047
Caroline Windell <cwindell@gmail.com>
2017-03-03 10:22:05 -0800

    Add final files

---
+++ final/final.js
@@ -0,0 +1,18 @@
+//Final_CarolineWindell
+
+document.addEventListener ('DOMContentLoaded', function (){
+
+var divReg = document.getElementById('divRegistered');
+var divMain = document.getElementById('divMain');
+
+})
+
+function fHandleEnter(){
+
+
+}
+
+function fHandleExit(){
+
+	
+}

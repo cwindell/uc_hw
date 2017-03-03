a58e454d082401624d6aa7bdc6f7b904e8a241a8
Caroline Windell <cwindell@gmail.com>
2017-03-03 11:36:18 -0800

    Update final.js

--- final/final.js
+++ final/final.js
@@ -2,17 +2,18 @@
 
 document.addEventListener ('DOMContentLoaded', function (){
 
-var divReg = document.getElementById('divRegistered');
-var divMain = document.getElementById('divMain');
-
-})
-
-function fHandleEnter(){
-
-
-}
-
-function fHandleExit(){
-
+	var divReg = document.getElementById('divRegistered');
+	var divMain = document.getElementById('divMain');
 	
+	divMain.addEventListener('click', function (event) {fHandleEnter(event, this);}, true);
+	divMain.addEventListener('mouseout', function (event) {fHandleExit(event, this);}, true);
+
+});
+
+function fHandleEnter(e){
+	e.target.style.backgroundColor = "yellow";
+};
+
+function fHandleExit(e){
+	e.target.style.backgroundColor = "white";	
 }

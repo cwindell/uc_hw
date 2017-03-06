21bf33ba1293e10ea24da81c5c10bb347035248f
Caroline Windell <cwindell@gmail.com>
2017-03-05 22:22:06 -0800

    Add files via upload

---
+++ week6/hw6.js
@@ -0,0 +1,23 @@
+document.addEventListener ('DOMContentLoaded', function (){
+
+	var txt = document.getElementById('txtUsername');
+
+	txt.addEventListener('focus', function (event) {fTextInputGetFocus(event);}, true);
+	txt.addEventListener('blur', function (event) {fTextInputLostFocus(event);}, true);
+
+	function fTextInputGetFocus(){
+
+	txt =  
+
+	};
+
+	function fTextInputLostFocus(){
+		if (txt. length === 0){
+		txt = 'Enter Username';
+		border
+		} else {
+
+		};
+	};
+})
+
